7324c45ed2af51e3c43091630ca8afa41ac1760d
Nikhil Musale <nikhilmusale70@gmail.com>
2021-08-27 15:48:17 +0530

    [Nihkil] - added first letter caps, minimum 3 alphabets with regex

--- src/RegEx.java
+++ src/RegEx.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class RegEx {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your username :- ");
+        String name = sc.nextLine();
+        System.out.println(validUserName(name));
+    }
+
+    public static boolean validUserName(String name){
+        String regEx = "^[A-Z][A-Z a-z]\\w{1,10}$";
+        Pattern p = Pattern.compile(regEx);
+
+        if (name==null)
+            return false;
+        Matcher m = p.matcher(name);
+        return m.matches();
     }
 }
