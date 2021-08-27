cf16b9ddf0cd3d835804cdbe3cb54a406a277dde
Nikhil Musale <nikhilmusale70@gmail.com>
2021-08-27 15:52:55 +0530

    [Nihkil] - added last name validation

--- src/RegEx.java
+++ src/RegEx.java
@@ -5,9 +5,22 @@ import java.util.regex.Pattern;
 public class RegEx {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter your username :- ");
-        String name = sc.nextLine();
-        System.out.println(validUserName(name));
+        System.out.print("Enter your first name :- ");
+        String firstName = sc.nextLine();
+
+        System.out.println("Enter you last name");
+        String lastName = sc.nextLine();
+
+        if(validUserName(firstName))
+            System.out.println("First name is valid");
+        else
+            System.out.println("First name is not valid");
+
+        if (validUserName(lastName))
+            System.out.println("Last name is val");
+        else
+            System.out.println("Last name invalid");
+
     }
 
     public static boolean validUserName(String name){
