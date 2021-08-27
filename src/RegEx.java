33cddb7746e757c427b0c283afba72b5abbe4c02
Nikhil Musale <nikhilmusale70@gmail.com>
2021-08-27 22:02:53 +0530

    [Nikhil] - added email validation feature

--- src/RegEx.java
+++ src/RegEx.java
@@ -7,19 +7,25 @@ public class RegEx {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter your first name :- ");
         String firstName = sc.nextLine();
-
-        System.out.println("Enter you last name");
+        System.out.print("Enter you last name :- ");
         String lastName = sc.nextLine();
+        System.out.print("Enter your email id :-");
+        String email = sc.nextLine();
 
         if(validUserName(firstName))
             System.out.println("First name is valid");
         else
-            System.out.println("First name is not valid");
+            System.out.println("First name is invalid");
 
         if (validUserName(lastName))
-            System.out.println("Last name is val");
+            System.out.println("Last name is valid");
         else
             System.out.println("Last name invalid");
+
+        if (emailValidation(email))
+            System.out.println("email name is valid");
+        else
+            System.out.println("email name invalid");
 
     }
 
@@ -30,6 +36,16 @@ public class RegEx {
         if (name==null)
             return false;
         Matcher m = p.matcher(name);
+        return m.matches();
+    }
+
+    public static boolean emailValidation(String email){
+        String regEx = "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$";
+        Pattern p = Pattern.compile(regEx);
+
+        if (email == null)
+            return false;
+        Matcher m = p.matcher(email);
         return m.matches();
     }
 }
