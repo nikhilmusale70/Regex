b0ba5b75cdfc1f985db669c1bdb2826a951083e2
Nikhil Musale <nikhilmusale70@gmail.com>
2021-08-31 15:00:11 +0530

    [Nikhil] - added 1 numeric in password verification

--- src/RegEx.java
+++ src/RegEx.java
@@ -75,7 +75,7 @@ public class RegEx {
 
     public static boolean getPassword(String str)
     {
-        String reg="[a-z0-9[A-Z]+]{8,}";
+        String reg="[a-z[0-9]+[A-Z]+]{8,}";
         Pattern p=Pattern.compile(reg);
         return p.matcher(str).matches();
     }
